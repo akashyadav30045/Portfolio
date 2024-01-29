924fef9a4a4b7a0631f7f1740f7056f87792aa2d
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2024-01-29 20:36:13 +0530

    ..

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:akashdeveloper74@gmail.com">akashdeveloper74@gmail.com</a>
+          <a href="mailto:akashdeveloper74@gmail.com">Akash Yadav</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/akashyadav12/">linkedin.com/AkashYadav</a>
+          <a href="https://www.linkedin.com/in/akashyadav12/">linkedin.com/Akash</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/akashyadav30045">github.com/AkashYadav</a>
+          <a href="https://www.github.com/akashyadav30045">github.com/Akash</a>
         </li>
       </ul>
     </footer>
